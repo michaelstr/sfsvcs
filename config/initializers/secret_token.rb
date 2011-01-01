9749a235d263e7adaf784c2423fe260f03a9c941
Mike Strong <git@thestrongfamily.org>
2010-12-31 18:35:07 -0600

    First Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Sfsvcs::Application.config.secret_token = 'eee28ebae4614b54e8ea47cacc950d5324285927c9ed3c5b5bec29d9b16746a0bb8702c1fd8685c8940ac4503c0c9c023227c5dfed09f0adcca22eaa844649de'
